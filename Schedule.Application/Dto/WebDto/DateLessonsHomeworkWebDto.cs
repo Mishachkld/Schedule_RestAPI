6fafe1c4a3125993e22ddd604b488ed45bad5bc1
Mishach <mishachkld@gmail.com>
2024-08-03 11:24:21 +0200

    add Web Dto

---
+++ Schedule.Application/Dto/WebDto/DateLessonsHomeworkWebDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Schedule.Application.Dto.WebDto;
+
+public class DateLessonsHomeworkWebDto
+{
+    public Guid Id { get; set; }
+    public DateTime Day { get; set; }
+    public List<LessonHomeworkWebDto> DataDlh { get; set; }
+
+}
+
---
+++ Schedule.Application/Dto/WebDto/LessonHomeworkWebDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Schedule.Application.Dto.WebDto;
+
+public class LessonHomeworkWebDto
+{
+    public string Lesson { get; set; }
+    public string Homework { get; set; }
+    public int NumberLesson { get; set; }
+}
